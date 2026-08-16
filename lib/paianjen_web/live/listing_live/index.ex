acfca0e3f3713ebbef42d0034bbcd8a3930bf0ca
Laurentiu Dancu <laue.dancu@gmail.com>
2026-08-16 10:29:24 +0300

    Ban rebs and elite as private.

--- lib/paianjen_web/live/listing_live/index.ex
+++ lib/paianjen_web/live/listing_live/index.ex
@@ -712,7 +712,7 @@ defmodule PaianjenWeb.ListingLive.Index do
             <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M5 10l7-7m0 0l7 7m-7-7v18" />
             </svg>
-            Final
+            Ultim.
           </button>
 
           <button
